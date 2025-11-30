1bd5f52a2bee8db790aa025237a9354a9c4bb016
sh1var3ddy <srajamuri@gmail.com>
2025-11-29 20:58:28 -0500

    basic redux store setup

--- goodreadsFrontend/src/main.jsx
+++ goodreadsFrontend/src/main.jsx
@@ -1,12 +1,11 @@
 import './index.css';
-
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from 'Redux/Slices/store';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>
 );
