27aca841e4f1e46ad78b1be83c53c54c0397290a
sh1var3ddy <srajamuri@gmail.com>
2025-12-02 13:59:53 -0500

    removed comments

--- goodreadsFrontend/src/Routes/MainRoutes.jsx
+++ goodreadsFrontend/src/Routes/MainRoutes.jsx
@@ -1,11 +1,4 @@
 import {Route, Routes} from "react-router-dom";
-// import Home from "Pages/Home.jsx";
-// import Signup from "Pages/Auth/Signup.jsx";
-// import SignIn from "Pages/Auth/Signin.jsx";
-// import NotFound from "Pages/NotFound.jsx";
-// import Dashboard from "Pages/Dashboard";
-// import BookDescription from "Pages/BookDescription";
-// import Shelf from "Pages/Shelf";
 import { lazy } from "react";
 
 const Home = lazy(()=>import("Pages/Home.jsx"));
